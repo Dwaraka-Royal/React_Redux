840db5ca6d61d08c562f5563b47a50ab68ccc5af
Dwaraka-Royal <ummadisettidwaraka@gmail.com>
2021-07-30 10:39:27 +0530

    using axios to get users data from api

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,33 @@
-const Contact = () =>{
-    return(
+import React,{ Component} from 'react';
+import axios from 'axios';
+
+class Contact extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            user:[]
+        }
+    }
+
+    componentDidMount(){
+        axios.get('https://run.mocky.io/v3/619850c8-8785-408f-8690-bdfb678e006e').then(
+            res => this.setState({user:res.data.data})
+        )
+        
+    }
+
+    render(){
+        
+        return(
         <div>
             <h1>Hey iam Contact</h1>
+            <ul>
+                {this.state.user.map((user) =>{
+                    return <li key={user.id}>{ user.email}</li>
+                })}
+            </ul>
         </div>
     )
-}
+}}
 
 export default Contact;
